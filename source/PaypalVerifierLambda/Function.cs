dd5e7659ba81ee710bff3be9ca4d8eab4209a3db
kjpgit <kjpgit@users.noreply.github.com>
2019-01-25 10:06:01 -0600

    use async

--- source/PaypalVerifierLambda/Function.cs
+++ source/PaypalVerifierLambda/Function.cs
@@ -8,24 +8,26 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+using Amazon.SQS;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace PaypalVerifierLambda
 {
     public class Function
     {
-        public string FunctionHandler(Amazon.Lambda.SNSEvents.SNSEvent input, ILambdaContext context)
+        public async Task FunctionHandler(SNSEvent input, ILambdaContext context)
         {
             string verifiedSQS = Environment.GetEnvironmentVariable("PPOM_VERIFIED_SQS");
             string errorSQS = Environment.GetEnvironmentVariable("PPOM_ERROR_SQS");
             string verifyURL = Environment.GetEnvironmentVariable("PPOM_VERIFY_URL");
             string message = input.Records[0].Sns.Message;
 
-            Trace.Assert(verifiedSQS != null);
-            Trace.Assert(errorSQS != null);
-            Trace.Assert(verifyURL != null);
+            Trace.Assert(!String.IsNullOrWhiteSpace(verifiedSQS));
+            Trace.Assert(!String.IsNullOrWhiteSpace(errorSQS));
+            Trace.Assert(!String.IsNullOrWhiteSpace(verifyURL));
+            Trace.Assert(!String.IsNullOrWhiteSpace(message));
 
             string bodyRequest = "cmd=_notify-validate&" + message;
             Console.WriteLine($"SNS message: {bodyRequest}");
@@ -34,23 +36,23 @@ namespace PaypalVerifierLambda
             // Purposefully not trying to cache or reuse this client.
             // It's a Lambda and very low traffic.
             using (var client = new HttpClient()) {
+                // NB: Paypal profile configured to use UTF8.
+                // Tested with unicode characters in a product name.
                 var bytes = UTF8Encoding.UTF8.GetBytes(bodyRequest);
                 var content = new ByteArrayContent(bytes);
-                var result = client.PostAsync(verifyURL, content).Result;
+                var result = await client.PostAsync(verifyURL, content);
                 result.EnsureSuccessStatusCode();
-                var resultString = result.Content.ReadAsStringAsync().Result;
+                var resultString = await result.Content.ReadAsStringAsync();
                 Console.WriteLine($"verify result: {result.StatusCode}, {resultString}");
 
                 // Add to SQS queue
-                var sqsClient = new Amazon.SQS.AmazonSQSClient();
+                var sqsClient = new AmazonSQSClient();
                 if (resultString == "VERIFIED") {
-                    var response = sqsClient.SendMessageAsync(verifiedSQS, message).Result;
+                    await sqsClient.SendMessageAsync(verifiedSQS, message);
                 } else {
-                    var response = sqsClient.SendMessageAsync(errorSQS, message).Result;
+                    await sqsClient.SendMessageAsync(errorSQS, message);
                 }
             }
-
-            return "task finished";
         }
     }
 }
